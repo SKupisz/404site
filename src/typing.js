0f1a016d0e3ab1dd65afcaf54e269dc40e55df10
SKupisz <szym-ku@wp.pl>
2018-12-01 20:39:53 +0100

    basic commit

---
+++ src/typing.js
@@ -0,0 +1,140 @@
+var typing = new Typed("#error1", {
+  strings: ["4 4 4 4 4 4 4 4 4 4"],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 6500
+});
+var typing = new Typed("#error2", {
+  strings: [" 0 0 0 0 0 0 0 0 0 "],//9
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 6600
+});
+var typing = new Typed("#error3", {
+  strings: ["4 4 4 4 4 4 4 4"],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 6700
+});
+var typing = new Typed("#error4", {
+  strings: [" 0 0 0 0 0 0 0 "],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 6800
+});
+var typing = new Typed("#error5", {
+  strings: ["4 4 4 4 4 4"],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 6900
+});
+var typing = new Typed("#error6", {
+  strings: [" 0 0 0 0 0 "],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 7000
+});
+var typing = new Typed("#error7", {
+  strings: ["4 4 4 4"],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 7100
+});
+var typing = new Typed("#error8", {
+  strings: [" 0 0 0 "],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 7200
+});
+var typing = new Typed("#error9", {
+  strings: ["4 4"],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 7300
+});
+var typing = new Typed("#error10", {
+  strings: [" 0 "],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 7400
+});
+var typing = new Typed("#error11", {
+  strings: [" 0 "],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 7400
+});
+var typing = new Typed("#error20", {
+  strings: ["4 4 4 4 4 4 4 4 4 4"],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 6500
+});
+var typing = new Typed("#error19", {
+  strings: [" 0 0 0 0 0 0 0 0 0 "],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 6600
+});
+var typing = new Typed("#error18", {
+  strings: ["4 4 4 4 4 4 4 4"],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 6700
+});
+var typing = new Typed("#error17", {
+  strings: [" 0 0 0 0 0 0 0 "],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 6800
+});
+var typing = new Typed("#error16", {
+  strings: ["4 4 4 4 4 4"],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 6900
+});
+var typing = new Typed("#error15", {
+  strings: [" 0 0 0 0 0 "],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 7000
+});
+var typing = new Typed("#error14", {
+  strings: ["4 4 4 4"],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 7100
+});
+var typing = new Typed("#error13", {
+  strings: [" 0 0 0 "],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 7200
+});
+var typing = new Typed("#error12", {
+  strings: ["4 4"],
+  stringsElement: null,
+  typeSpeed: 35,
+  backSpeed: 35,
+  startDelay: 7300
+});
